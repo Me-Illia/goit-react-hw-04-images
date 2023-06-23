2767874a7bcaedc5dcbe28668a784f9b11b3583c
Me-Illia <sq999@ukr.net>
2023-06-23 13:27:16 +0300

    Update Searchbar.styled.js

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -18,7 +18,7 @@ export const Header = styled.header`
   color: #fff;
   background: linear-gradient(90deg, rgb(79 36 240 / 83%) 48%, rgb(32 93 209 / 22%) 64%);
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+    0px 4px 5px 0px rgba(0, 0, 0, 0.13), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
 
 export const Form = styled.form`
